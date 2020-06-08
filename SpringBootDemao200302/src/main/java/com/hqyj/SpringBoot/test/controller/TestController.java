6f675e77c44342e2aa98398d4c3303fc69154b6a
ASUS <ASUS@DESKTOP-65N77TE>
2020-06-08 13:38:56 +0800

    "引入thymeleaf、页面模块化，碎片化，页面数据处理"

--- SpringBootDemao200302/src/main/java/com/hqyj/SpringBoot/test/controller/TestController.java
+++ SpringBootDemao200302/src/main/java/com/hqyj/SpringBoot/test/controller/TestController.java
@@ -1,13 +1,21 @@
 package com.hqyj.SpringBoot.test.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hqyj.SpringBoot.test.entity.City;
+import com.hqyj.SpringBoot.test.entity.Country;
+import com.hqyj.SpringBoot.test.service.CityService;
+import com.hqyj.SpringBoot.test.service.CountryService;
 import com.hqyj.SpringBoot.test.vo.ApplicationTest;
 
 @Controller
@@ -28,7 +36,44 @@ public class TestController {
 	private ApplicationTest ApplicationTest;
 	
 	private static final Logger LOGGR = LoggerFactory.getLogger(TestController.class);
-
+	@Autowired
+	private CityService cityService;
+	@Autowired
+	private CountryService countryService;
+	/**
+	 * 127.0.0.1/test/index
+	 */
+	
+	/**
+	 * 127.0.0.1/test/index
+	 */
+	@RequestMapping("/index")
+	public String indexPage(ModelMap modelmap) {
+		int countryId = 522;
+		List<City> cities = cityService.getCitiesByCountryId(countryId);
+		cities = cities.stream().limit(10).collect(Collectors.toList());
+		Country country = countryService.getCountryByCountryId(countryId);
+		
+		modelmap.addAttribute("thymeleafTitle", "scdscsadcsacd");
+		modelmap.addAttribute("checked", true);
+		modelmap.addAttribute("currentNumber", 99);
+		modelmap.addAttribute("changeType", "checkbox");
+		modelmap.addAttribute("baiduUrl", "/test/log");
+		modelmap.addAttribute("city", cities.get(0));
+		modelmap.addAttribute("shopLogo", 
+				"http://cdn.duitang.com/uploads/item/201308/13/20130813115619_EJCWm.thumb.700_0.jpeg");
+		modelmap.addAttribute("country", country);
+		modelmap.addAttribute("cities", cities);
+		modelmap.addAttribute("updateCityUri", "/api/city");
+		modelmap.addAttribute("template", "test/index");
+		
+		return "index";
+	}
+	
+	
+	/**
+	 * 127.0.0.1/test/log
+	 */
 	@RequestMapping("/log")
 	@ResponseBody
 	public String logTest() {
