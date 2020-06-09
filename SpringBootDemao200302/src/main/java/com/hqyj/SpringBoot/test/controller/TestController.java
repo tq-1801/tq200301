f264f779462d70a4326f0d88064bc554f473a463
ASUS <ASUS@DESKTOP-65N77TE>
2020-06-09 13:17:52 +0800

    "文件上传"

--- SpringBootDemao200302/src/main/java/com/hqyj/SpringBoot/test/controller/TestController.java
+++ SpringBootDemao200302/src/main/java/com/hqyj/SpringBoot/test/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.hqyj.SpringBoot.test.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,9 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.hqyj.SpringBoot.test.entity.City;
 import com.hqyj.SpringBoot.test.entity.Country;
@@ -44,7 +49,58 @@ public class TestController {
 	@Autowired
 	private CountryService countryService;
 	
-	
+	@PostMapping(value = "/files", consumes = "multipart/form-data")
+	public String uploadFiles(@RequestParam MultipartFile[] files, RedirectAttributes redirectAttributes) {
+		boolean isEmpty = true;
+		for (MultipartFile file : files) {
+			if (file.isEmpty()) {
+				continue;
+			}
+
+			try {
+				String destFilePath = "F:\\upload\\" + file.getOriginalFilename();
+				File destFile = new File(destFilePath);
+				file.transferTo(destFile);
+
+				isEmpty = false;
+			} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+				redirectAttributes.addFlashAttribute("message", "Upload fail.");
+				return "redirect:/test/index";
+			}
+
+		}
+
+		if (isEmpty) {
+			redirectAttributes.addFlashAttribute("message", "Please select file");
+		} else  {
+			redirectAttributes.addFlashAttribute("message", "Upload success.");
+		}
+
+		return "redirect:/test/index";
+	}
+
+	@PostMapping(value = "/file", consumes = "multipart/form-data")
+	public String uploadFile(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
+
+		if (file.isEmpty()) {
+			redirectAttributes.addFlashAttribute("message", "Please select file");
+			return "redirect:/test/index";
+		}
+
+		try {
+			String destFilePath = "F:\\upload\\" + file.getOriginalFilename();
+			File destFile = new File(destFilePath);
+			file.transferTo(destFile);
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("message", "Upload fail.");
+			return "redirect:/test/index";
+		}
+
+		redirectAttributes.addFlashAttribute("message", "Upload success.");
+		return "redirect:/test/index";
+	}
 	/**
 	 * 127.0.0.1/test/index
 	 */
